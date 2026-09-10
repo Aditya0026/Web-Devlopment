00f3c849d996b4a096f1c0ffd77f0ed1b76bada1
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-10 07:20:33 +0530

    Function in JavaScript

---
+++ Section 14/14.03-Function/CreatingFunction.js
@@ -0,0 +1,22 @@
+function getMilk(){
+console.log("leaving home");
+console.log("moving right");
+console.log("moving right");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("move right");
+console.log("move right");
+console.log("buy Milk");
+console.log("move left");
+console.log("move left");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("move left");
+console.log("move left");
+console.log("Enter Home");
+}
+getMilk();

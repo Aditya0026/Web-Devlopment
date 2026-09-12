3e33c8430b8612fcb4016bc8ec5e0e04a755911b
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-13 02:02:10 +0530

    Parameter and argument in function using JavaScript

---
+++ Section 14/14.03-Function/Parameter-argument.js
@@ -0,0 +1,25 @@
+function getMilk(money){
+console.log("leaving home");
+console.log("moving right");
+console.log("moving right");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("move right");
+console.log("move right");
+
+    var numberOfBotteles = Math.floor(money / 1.5); 
+console.log("buy "+ numberOfBotteles + " bottles of Milk");
+
+console.log("move left");
+console.log("move left");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("move left");
+console.log("move left");
+console.log("Enter Home");
+}
+getMilk(10);

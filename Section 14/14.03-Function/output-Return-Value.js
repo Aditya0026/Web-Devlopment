5f268b46f6138b6948f7e3954d939b5c8350592d
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-13 13:15:35 +0530

    function output and Return Value

---
+++ Section 14/14.03-Function/output-Return-Value.js
@@ -0,0 +1,40 @@
+function getMilk(money){
+console.log("leaving home");
+console.log("moving right");
+console.log("moving right");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("moveUp");
+console.log("move right");
+console.log("move right");
+
+console.log("buy "+ calcBottles(money, 1.5) + " bottles of Milk");
+
+    
+console.log("move left");
+console.log("move left");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("moveDown");
+console.log("move left");
+console.log("move left");
+console.log("Enter Home");
+
+    return calcChange(money, 1.5);
+    
+}
+
+function calcBottles(startingmoney, costPerBottle){
+var numberOfBotteles = Math.floor(startingmoney / costPerBottle);
+return numberOfBotteles;
+}
+
+
+function calcChange(startingAmount, costPerBottele){
+    var Change = startingAmount % costPerBottele;
+return Change;
+}
+
+console.log("Hello master, here is your" + getMilk(10, 3) + "change.")

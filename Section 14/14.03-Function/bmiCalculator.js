e95a757ea9839249771e222fdbec4547773daa64
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-13 15:01:50 +0530

    Math.Random function

--- Section 14/14.03-Function/bmiCalculator.js
+++ Section 14/14.03-Function/bmiCalculator.js
@@ -2,7 +2,10 @@ function bmiCalculator(weight, height) {
 
    // var bmi = weight / (height * height);
    var bmi = weight / Math.pow(height, 2);
-    return Math.round(bmi);
+
+   // Math.round will give the output if 3.5 so it will return 4
+   // if 3.4 so ouput will be 3 
+    return Math.round(bmi); 
 }
 var bmi = bmiCalculator(65, 2);
 console.log(bmi);
---
+++ Section 15/15.01-IntermediateJavaScript.js
@@ -0,0 +1,5 @@
+// Math.random function it will return random value or output 
+var n = Math.random();
+var n = n * 6;
+n = Math.floor(n) + 1;
+console.log(n);

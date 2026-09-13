cc13c39efe752ea5d1cb4ab9d0c8e412b3684d24
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-13 14:15:26 +0530

    working on bmi Calculator

--- Section 14/14.03-Function/bmiCalculator.js
+++ Section 14/14.03-Function/bmiCalculator.js
@@ -2,7 +2,7 @@ function bmiCalculator(weight, height) {
 
    // var bmi = weight / (height * height);
    var bmi = weight / Math.pow(height, 2);
-    return(bmi);
+    return Math.round(bmi);
 }
-var bmi = bmiCalculator(65, 4);
+var bmi = bmiCalculator(65, 2);
 console.log(bmi);

5c3eb6be4149e3926296ff26f743c3ddd0bac5de
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-16 19:12:02 +0530

    Data Type

---
+++ Section 16/16.01-Basics/16.02-DataTypes.js
@@ -0,0 +1,15 @@
+console.log(3 + 3);
+console.log("DATAtype");
+
+let name = "Deepak";
+let age = 23;
+let isLoogedIn = false;
+console.table([name, age, isLoogedIn]);
+
+console.log(typeof"Deepak");
+console.log(typeof 23);
+console.log(typeof true);
+console.log(typeof null); // note when we check type of null we will get object 
+console.log(typeof undefined); // but here when we are checking type of undefined we are getting undefined
+
+

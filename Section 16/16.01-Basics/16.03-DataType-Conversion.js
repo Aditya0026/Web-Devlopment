a6ae8bea796a137232bf596ac00e4d27c5ca28aa
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-16 19:55:50 +0530

    Conversion of Data Type

---
+++ Section 16/16.01-Basics/16.03-DataType-Conversion.js
@@ -0,0 +1,24 @@
+// Conversion of Datatype
+let score = null;
+console.log(typeof score);
+console.log(typeof (score)); 
+
+
+let change = Number(score);
+console.log(typeof(change));
+console.log(change);
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+ 
+//  1 => true; 0 => false
+// "" => false
+// "Aditya" => true
+
+
+let somenumber = 35;
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);

bc0f72e872c0624718c8d41b8190b552a9f929c2
Aditya Kumar <kumaraditya74358@gmail.com>
2026-09-17 02:01:28 +0530

    DataType-Conversion

--- Section 16/16.01-Basics/16.03-DataType-Conversion.js
+++ Section 16/16.01-Basics/16.03-DataType-Conversion.js
@@ -1,19 +1,21 @@
-//
+// DataType Conversion
 //  1 => true; 0 => false
 // "" => false
 // "Aditya" => true
 
 
-// let somenumber = 35;
-// let stringnumber = String(somenumber);
+let somenumber = 35;
+let stringnumber = String(somenumber);
 // console.log(stringnumber);
 // console.log(typeof stringnumber);
 
 
 //****************xxxxxxx       Opration           xxxxxxxxxx****************/
 
-// let value = 4;
-// let negValue = -value;
+
+
+let value = 4;
+let negValue = -value;
 // console.log(negValue);
 
 // console.log(2 + 3); 
@@ -24,11 +26,11 @@
 // console.log(2 ** 3);
 
 
-// let str1 = "Hello";
-// let str2 = " Aditya";
+let str1 = "Hello";
+let str2 = " Aditya";
 
-// let str3 = str1 + str2;
-// console.log(str3);
+let str3 = str1 + str2;
+//console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
@@ -38,3 +40,13 @@
 
 console.log((2 + 3 ) * 4 / 5);
 console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 =  5 + 2;
+console.log(num1);
+
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
